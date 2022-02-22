ff0d7ae67383df151b34afc06f4eb26508b263de
Sam Lombardo <sam.lombardo@awesam.net>
2022-02-22 12:18:59 -0400

    Add solution

---
+++ src/problems/0643-maximum-average-subarray-i/Solution.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework.Constraints;
+
+namespace leetcode.problems.MaximumAverageSubarrayI
+{
+    public class Solution
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            double? currentLargestAverage = null;
+            double runningTotal = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                runningTotal += nums[i];
+                if (i < k - 1) continue;
+                if (i >= k) runningTotal -= nums[i - (k)];
+
+                var average = runningTotal / k;
+                if (!currentLargestAverage.HasValue || currentLargestAverage < average) currentLargestAverage = average;
+            }
+
+            return currentLargestAverage ?? 0;
+        }
+    }
+}
