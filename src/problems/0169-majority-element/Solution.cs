7e3660aecde2ba71268bb3030e3e7415ca4b116f
Sam Lombardo <sam.lombardo@awesam.net>
2022-03-01 15:54:24 -0400

    Add solution

---
+++ src/problems/0169-majority-element/Solution.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework.Constraints;
+
+namespace leetcode.problems.MajorityElement
+{
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            var sorted = nums.OrderBy(x => x).ToArray();
+            return sorted[sorted.Length / 2];
+        }
+    }
+
+    public class Solution2
+    {
+        public int MajorityElement(int[] nums)
+        {
+            var counts = new Dictionary<int, int>();
+            var halfway = nums.Length / 2;
+
+            foreach (var number in nums)
+            {
+                var count = counts.GetValueOrDefault(number) + 1;
+                if (count > halfway) return number;
+
+                counts[number] = count;
+            }
+
+            throw new ArgumentException($"{nameof(nums)} does not have a majority.");
+        }
+    }
+}
