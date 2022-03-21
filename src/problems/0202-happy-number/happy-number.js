8f06bba5edbbae67039b277cb687370dc4fa640d
Sam Lombardo <sam.lombardo@awesam.net>
2022-03-21 17:45:17 -0300

    Add Solution

---
+++ src/problems/0202-happy-number/happy-number.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isHappy = function (n) {
+
+    const processed = new Set();
+    let current = n;
+
+    let failsafe = 100;
+    while (current !== 1 && --failsafe !== 0){
+        const stringValue = current.toString();
+
+        const processedCount = processed.size;
+        processed.add(current);
+
+        if (processedCount === processed.size) return false;
+
+        current = 0;
+
+        for (let i = 0; i < stringValue.length; i++) {
+            current += Math.pow(+stringValue[i], 2);
+        }
+
+        // console.log(`current is: ${current}`);
+    }
+
+    return true;
+};
+
+module.exports = {
+    isHappy: isHappy
+};
